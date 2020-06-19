c060eb748580e0cfb1c5ab9b1e4c67f249f97fdb
Justin Barnowski <jbarno54@gmail.com>
2020-06-19 00:28:43 -0500

    feat(all): got them all

--- python/embed.py
+++ python/embed.py
@@ -1,6 +1,7 @@
-import fire
 import aiohttp
 import asyncio
+import fire
+import json
 
 from pyfacebook import IgBasicApi
 
@@ -17,18 +18,21 @@ def user_by_token(token):
     resp = api.get_user_medias(user_id = user.id, count = None)
     return sorted(resp, key=lambda post: post.timestamp, reverse=True)
 
-async def _get(session, url):
-    async with session.get(url) as resp:
+async def _get(session, url, media):
+    params = { 'url' : media.permalink, 'hidecaption' : 0, 'omitscript' : 1}
+    async with session.get(url, params=params) as resp:
         return await resp.text()
 
-async def _embed(media_id):
+async def _embed(token):
+    medias = user_by_token(token)
     async with aiohttp.ClientSession() as session:
-        html = await _get(session, 'http://python.org')
-        print(html)
+        for media in medias:
+            text = await _get(session, 'https://api.instagram.com/oembed', media)
+            print(json.loads(text)['html'])
 
 def get_html(token):
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(python())
+    loop.run_until_complete(_embed(token))
 
 if __name__ == '__main__':
     fire.Fire()
