037474c7392f16c27bcad4d668315d7be365190f
Justin Barnowski <jbarno54@gmail.com>
2020-06-18 23:07:35 -0500

    feat(aiohttp): getting the html from yet another api

--- python/embed.py
+++ python/embed.py
@@ -1,5 +1,6 @@
 import fire
 import aiohttp
+import asyncio
 
 from pyfacebook import IgBasicApi
 
@@ -15,6 +16,19 @@ def user_by_token(token):
     user = api.get_user_info()
     resp = api.get_user_medias(user_id = user.id, count = None)
     return sorted(resp, key=lambda post: post.timestamp, reverse=True)
+
+async def _get(session, url):
+    async with session.get(url) as resp:
+        return await resp.text()
+
+async def _embed(media_id):
+    async with aiohttp.ClientSession() as session:
+        html = await _get(session, 'http://python.org')
+        print(html)
+
+def get_html(token):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(python())
 
 if __name__ == '__main__':
     fire.Fire()
