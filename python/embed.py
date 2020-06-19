df8fcd921ef1fc5846e6df22650d93061f6ed93b
Justin Barnowski <jbarno54@gmail.com>
2020-06-18 22:47:54 -0500

    feat(scrap): works to grab all posts

--- python/embed.py
+++ python/embed.py
@@ -6,14 +6,15 @@ from pyfacebook import IgBasicApi
 def hello(world = "World"):
     return "Hello " + world
 
-def user(token, uid = ''):
-    print("Token length: {len}!".format(len=len(token)))
-    print("Userid: {uid}".format(uid=uid))
-    api = IgBasicApi(long_term_token=token)
-    print("Operating as:")
-    print(api.get_user_info())
-    resp = api.get_user_medias(user_id=uid)
-    return resp
+def user_by_token(token):
+    '''
+    Given a user long lived token, generate a list of
+    all media posts sorted by newest first.
+    '''
+    api = IgBasicApi(long_term_token = token)
+    user = api.get_user_info()
+    resp = api.get_user_medias(user_id = user.id, count = None)
+    return sorted(resp, key=lambda post: post.timestamp, reverse=True)
 
 if __name__ == '__main__':
     fire.Fire()
