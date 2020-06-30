2c4a66ec7ee5956a5e778c85ed20b89cd465c745
Justin Barnowski <jbarno54@gmail.com>
2020-06-30 00:07:53 -0500

    feat(refresh): uses existing token to refresh token and writeback

--- python/refresh.py
+++ python/refresh.py
@@ -6,29 +6,30 @@ import os
 
 
 async def _get(session, url, token):
-    params = {grant_type: "ig_refresh_token", access_token: token}
-    async with session.get(url, params) as resp:
-        assert resp.ok, resp.message
+    params = {"grant_type": "ig_refresh_token", "access_token": token}
+    async with session.get(url, params=params) as resp:
         return await resp.text()
 
 
 async def _refresh_token(path):
-    data = None
-    with open(path) as f:
+    with open(path, "r") as f:
         data = f.read()
     assert data, "No data found at path"
 
-    with aiohttp.ClientSession() as session:
-        html = await _get(
+    async with aiohttp.ClientSession(raise_for_status=True) as session:
+        resp = await _get(
             session, "https://graph.instagram.com/refresh_access_token", data
         )
-        print(html)
+        token = json.loads(resp)['access_token']
 
+    with open(path, "w") as f:
+        f.write(token)
+        f.truncate()
 
 def refresh_token(path):
     print("Attempting to refresh token at: {}".format(path))
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(refresh_token(path))
+    loop.run_until_complete(_refresh_token(path))
     print("Token at: {} refreshed.".format(path))
 
 
