5015512e4ca9d10894c587472f89ba8fb5b3e9d5
Justin Barnowski <jbarno54@gmail.com>
2020-07-04 10:29:05 -0500

    feat(latest): get only latest comic
    also had to move py-facebook-api to run with master

--- python/embed.py
+++ python/embed.py
@@ -13,7 +13,7 @@ def hello(world="World"):
     return "Hello " + world
 
 
-def _user_by_token(token, count):
+def _user_by_token(token, count=None):
     """
     Given a user long lived token, generate a list of
     all media posts sorted by newest first.
@@ -30,8 +30,8 @@ async def _get(session, url, media):
         return await resp.text()
 
 
-async def _embed(token):
-    medias = _user_by_token(token)
+async def _embed(token, count=None):
+    medias = _user_by_token(token, count=count)
     async with aiohttp.ClientSession() as session:
         for media in medias:
             text = await _get(session, "https://api.instagram.com/oembed", media)
@@ -43,7 +43,7 @@ def get_all_html(token):
     Given a long user token spit out all their posts as embedable html
     """
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(_embed(token), count=None)
+    loop.run_until_complete(_embed(token))
 
 
 def get_latest_html(token):
@@ -51,7 +51,7 @@ def get_latest_html(token):
     Given a long user token spit their latest posts as embedable html
     """
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(_embed(token), count=1)
+    loop.run_until_complete(_embed(token, count=1))
 
 
 if __name__ == "__main__":
