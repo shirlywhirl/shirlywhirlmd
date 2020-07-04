3299adac2c0b42d0ba51bf95f406c43bbaf37ee9
Justin Barnowski <jbarno54@gmail.com>
2020-07-04 09:41:55 -0500

    feat(get_1): just get the latest, hopefully

--- python/embed.py
+++ python/embed.py
@@ -5,40 +5,54 @@ import json
 
 from pyfacebook import IgBasicApi
 
-def hello(world = "World"):
-    '''
+
+def hello(world="World"):
+    """
     Say Hello to fire.
-    '''
+    """
     return "Hello " + world
 
-def _user_by_token(token):
-    '''
+
+def _user_by_token(token, count):
+    """
     Given a user long lived token, generate a list of
     all media posts sorted by newest first.
-    '''
-    api = IgBasicApi(long_term_token = token)
+    """
+    api = IgBasicApi(long_term_token=token)
     user = api.get_user_info()
-    resp = api.get_user_medias(user_id = user.id, count = None)
-    return sorted(resp, key = lambda post: post.timestamp, reverse=True)
+    resp = api.get_user_medias(user_id=user.id, count=count)
+    return sorted(resp, key=lambda post: post.timestamp, reverse=True)
+
 
 async def _get(session, url, media):
-    params = { 'url' : media.permalink, 'hidecaption' : 0, 'omitscript' : 1}
+    params = {"url": media.permalink, "hidecaption": 0, "omitscript": 1}
     async with session.get(url, params=params) as resp:
         return await resp.text()
+
 
 async def _embed(token):
     medias = _user_by_token(token)
     async with aiohttp.ClientSession() as session:
         for media in medias:
-            text = await _get(session, 'https://api.instagram.com/oembed', media)
-            print(json.loads(text)['html'])
+            text = await _get(session, "https://api.instagram.com/oembed", media)
+            print(json.loads(text)["html"])
 
-def get_html(token):
-    '''
+
+def get_all_html(token):
+    """
     Given a long user token spit out all their posts as embedable html
-    '''
+    """
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(_embed(token))
+    loop.run_until_complete(_embed(token), count=None)
 
-if __name__ == '__main__':
+
+def get_latest_html(token):
+    """
+    Given a long user token spit their latest posts as embedable html
+    """
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(_embed(token), count=1)
+
+
+if __name__ == "__main__":
     fire.Fire()
--- python/refresh.py
+++ python/refresh.py
@@ -2,18 +2,22 @@ import aiohttp
 import asyncio
 import fire
 
+
 async def _get(session, url):
     async with session.get(url) as resp:
         return await resp.text()
 
+
 async def python():
     async with aiohttp.ClientSession() as session:
-        html = await _get(session, 'http://python.org')
+        html = await _get(session, "http://python.org")
         print(html)
+
 
 def hello():
     loop = asyncio.get_event_loop()
     loop.run_until_complete(python())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     fire.Fire()
