f64e26da23fa7fee828c7857123986c072fda2e8
Justin Barnowski <jbarno54@gmail.com>
2020-08-24 20:39:09 -0500

    feat(categories): via substring

--- tools/make_tags.py
+++ tools/make_tags.py
@@ -3,6 +3,45 @@
 import os
 import re
 
+CATEGORICAL_SUBSTR = ['logy',
+'obgyn',
+'surgery',
+'pediatrics',
+'peds',
+'familymed',
+'internalmedicine',
+'corona',
+'ppe',
+'resident',
+'intern',
+'inpatient',
+'outpatient',
+'attending',
+'medstudent',
+'doctor',
+'admin',
+'nurse',
+'black',
+'blm',
+'singlepayer',
+'relatable',
+'uchicago',
+'pritzker',
+'womeninmedicine',
+'graphicmedicine',
+'premed',
+'premedmotivation',
+'professionalism',
+'pgy',
+'ms',
+'step',
+'shelf',
+'opioid',
+'ivf',
+'mcat',
+'uworld']
+
+
 def main():
     path = '/mnt/src/shirlywhirlmd/_posts/'
     for f in os.listdir(path):
@@ -10,14 +49,16 @@ def main():
             with open(os.path.join(path,f), 'r') as post:
                 data = post.read()
 
-
             print(f)
             for word in re.split('#', data):
-                if len(word) > 25:
+                if len(word) > 25 or len(word) <= 2:
+                    continue
+		if not word.strip().isalnum():
                     continue
                 print("tag: {}".format(word.strip()))
-                if 'logy' in word:
-                    print("categorical")
+                for w in CATEGORICAL_SUBSTR:
+                    if w in word:
+                       print("categorical via : {}".format(w))
 
 
 
