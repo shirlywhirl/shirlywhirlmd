415ce8be660077f26302a3a171f18318c800baf6
Justin Barnowski <jbarno54@gmail.com>
2020-08-24 22:38:45 -0500

    feat(tags): get them the dumb way

--- tools/make_tags.py
+++ tools/make_tags.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+from collections import defaultdict
 
 CATEGORICAL_SUBSTR = ['logy',
 'obgyn',
@@ -41,26 +42,49 @@ CATEGORICAL_SUBSTR = ['logy',
 'mcat',
 'uworld']
 
-
 def main():
     path = '/mnt/src/shirlywhirlmd/_posts/'
     for f in os.listdir(path):
-        if f.endswith('.md'):
-            with open(os.path.join(path,f), 'r') as post:
-                data = post.read()
+        if not f.endswith('.md'):
+            continue
+        print("processing: {}".format(f))
+        post_path = os.path.join(path, f)
+        with open(post_path,  'r') as opost:
+            data = opost.readlines()
 
-            print(f)
-            for word in re.split('#', data):
+        tags = set()
+        cat = set()
+
+        for line in data:
+            for word in re.split('#', line):
                 if len(word) > 25 or len(word) <= 2:
                     continue
-		if not word.strip().isalnum():
+                if not word.strip().isalnum():
                     continue
-                print("tag: {}".format(word.strip()))
+                word = word.strip()
+                tags.add(word.lower())
                 for w in CATEGORICAL_SUBSTR:
                     if w in word:
-                       print("categorical via : {}".format(w))
+                        cat.add(word.title())
+        tags = tags
+        cat = cat
 
+        for index in range(0, len(data)):
+            data[index] = re.sub('categories: \[Medicine\]', 'categories: {}'.format(list(cat)), data[index])
+            data[index] = re.sub('tags: \[comic\]', 'tags: {}'.format(list(tags)), data[index])
+        #data = _process_unit(data,'categories: [Medicine]', 'categories', cat)
+        #data = _process_unit(data,'tags: [comic]', 'tags', tags)
 
+        with open(post_path, 'w') as post:
+            post.writelines(data)
+
+def _process_unit(lines, old, key, value):
+    for data in lines:
+        if any(value):
+            data.replace(old, '{}: {}'.format(key, value))
+        else:
+            data.replace(old, '{}: no_{}'.format(key, key))
+    return lines
 
 if __name__ == '__main__':
     main()
