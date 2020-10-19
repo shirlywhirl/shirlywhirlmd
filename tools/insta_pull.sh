b3815f34714fb02248bd90bc33daaa4fd399bba0
Justin Barnowski <jbarno54@gmail.com>
2020-10-18 22:55:06 -0500

    feat(install): see if install works

---
+++ tools/insta_pull.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Initial the Categories/Tags pages and Lastmod for posts and then push to remote
+#
+# v2.0
+# https://github.com/cotes2020/jekyll-theme-chirpy
+# © 2019 Cotes Chung
+# Published under MIT License
+
+set -eu
+
+WORK_DIR="$(dirname $(dirname $(realpath "$0")))"
+echo $WORK_DIR
+
+cd python
+poetry install .
+poetry show
