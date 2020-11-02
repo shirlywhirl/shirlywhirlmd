bda22c1afabaf1c0ce4b20710f03e4768c9938ab
Justin Barnowski <jbarno54@gmail.com>
2020-11-01 20:43:12 -0600

    --wip-- [skip ci]

--- tools/local.sh
+++ tools/local.sh
@@ -4,6 +4,14 @@ set -e
 echo "Doing pythonic things"
 pushd ../python
 poetry run embed_skip.py "${ENV_INSTA_TOKEN}"
+<<<<<<< Updated upstream
 popd
+=======
+
+echo "Doing chirpy things"
+pushd tools
+
+
+>>>>>>> Stashed changes
 
 echo "As you were"
